a7d014992d56c307c6b3bbd5610596a44d1143f2
Denise Williams <williamsde@outlook.com>
2016-06-15 08:50:22 -0400

    attempt code for homework

--- GoWithTheFlow/Program.cs
+++ GoWithTheFlow/Program.cs
@@ -10,6 +10,20 @@ namespace GoWithTheFlow
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine ("Does the computer turn on?");
+            Console.WriteLine("Please answer yes or no");
+            Console.ReadLine();
+            string answer = Console.ReadLine();
+            if (answer == "yes")
+                {
+                Console.WriteLine("Great!");
+            }
+            else if(answer == "no")
+                            {
+                Console.WriteLine("Your computer is unplugged");
+            }
+            Console.ReadKey();
         }
     }
 }
